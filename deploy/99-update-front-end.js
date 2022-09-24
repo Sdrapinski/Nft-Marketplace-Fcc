d352945ac9cc0286918cdc4451753f5ac4ae5542
Sdrapinski <szymondrapinski@gmail.com>
2022-09-24 23:19:21 +0200

    moralis server

---
+++ deploy/99-update-front-end.js
@@ -0,0 +1,38 @@
+const { ethers, network } = require("hardhat");
+const fs = require("fs");
+
+const frontEndContractFile =
+  "../nextjs-nft-marketplace/constants/networkMapping.json";
+
+module.exports = async function () {
+  if (process.env.UPDATE_FRONT_END) {
+    console.log("Updating front end");
+    await updateContractAddresses();
+  }
+};
+
+async function updateAbi() {
+  const nftMarketplace = await ethers.getContract("NftMarketplace");
+}
+
+async function updateContractAddresses() {
+  const nftMarketplace = await ethers.getContract("NftMarketplace");
+  const chainId = network.config.chainId.toString();
+  const contractAddresses = JSON.parse(
+    fs.readFileSync(frontEndContractFile, "utf8")
+  );
+  if (chainId in contractAddresses) {
+    if (
+      !contractAddresses[chainId]["NftMarketplace"].includes(
+        nftMarketplace.address
+      )
+    ) {
+      contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address);
+    }
+  } else {
+    contractAddresses[chainId] = { NftMarketplace: [nftMarketplace.address] };
+  }
+  fs.writeFileSync(frontEndContractFile, JSON.stringify(contractAddresses));
+}
+
+module.exports.tags = ["all", "frontend"];
