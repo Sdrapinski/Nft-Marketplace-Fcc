0eab000aa215d2d91ec8db5ffda00e1138584e80
Sdrapinski <szymondrapinski@gmail.com>
2022-09-26 00:01:11 +0200

    mine and mint

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -3,16 +3,27 @@ const fs = require("fs");
 
 const frontEndContractFile =
   "../nextjs-nft-marketplace/constants/networkMapping.json";
+const frontEndAbiLocation = "../nextjs-nft-marketplace/constants/";
 
 module.exports = async function () {
   if (process.env.UPDATE_FRONT_END) {
     console.log("Updating front end");
     await updateContractAddresses();
+    await updateAbi();
   }
 };
 
 async function updateAbi() {
   const nftMarketplace = await ethers.getContract("NftMarketplace");
+  fs.writeFileSync(
+    `${frontEndAbiLocation}NftMarketplace.json`,
+    nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
+  );
+  const basicNft = await ethers.getContract("BasicNft");
+  fs.writeFileSync(
+    `${frontEndAbiLocation}BasicNft.json`,
+    nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
+  );
 }
 
 async function updateContractAddresses() {
---
+++ scripts/mint.js
@@ -0,0 +1,22 @@
+const { ethers, network } = require("hardhat");
+const { moveBlocks } = require("../utils/move-blocks");
+
+async function mint() {
+  const basicNft = await ethers.getContract("BasicNft");
+  console.log("minting...");
+  const mintTx = await basicNft.mintNft();
+  const mintTxReceipt = await mintTx.wait(1);
+  const tokenId = mintTxReceipt.events[0].args.tokenId;
+  console.log(`Got token id ${tokenId}`);
+  console.log(`NFT address: ${basicNft.address}`);
+
+  if (network.config.chainId == "31337") {
+    await moveBlocks(2, (sleepAmount = 1000));
+  }
+}
+mint()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
